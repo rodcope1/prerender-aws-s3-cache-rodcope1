a4e22d4aa6cb6724a89a226047751fc2e4e8fedc
Rod Cope <rod@perfdog.com>
2024-05-28 13:43:50 -0600

    Moved to the latest version of the AWS SDK v3 (3.565.0) that still supports Node.js 14.x

--- index.js
+++ index.js
@@ -1,51 +1,60 @@
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3({params: {Bucket: process.env.S3_BUCKET_NAME}});
+import {S3} from "@aws-sdk/client-s3";
 const url = require('url');
 
+const s3 = new S3({
+  region: process.env.AWS_REGION,
+});
+
 module.exports = {
-  requestReceived: function(req, res, next) {
+  requestReceived: async function(req, res, next) {
     if (req.method !== 'GET') {
       return next();
     }
 
-    s3.getObject({Key: this.getCacheKey(req)}, (err, result) => {
-      if (!err && result) {
-        const freshness = process.env.S3_CACHE_FRESHNESS || 604800000;
-        const now = Date.now();
-        const lastModified = result.LastModified.getTime();
-        if ((now - lastModified) < freshness) {
-          return res.send(200, result.Body);
-        }
+    try {
+      const get = await s3.getObject({Bucket: process.env.S3_BUCKET_NAME, Key: this.getCacheKey(req)});
+      const html = await get.Body.transformToString();
+      const freshness = process.env.S3_CACHE_FRESHNESS || 604800000;
+      const now = Date.now();
+      const lastModified = new Date(get.LastModified).getTime();
+      if ((now - lastModified) < freshness) {
+        return res.send(200, html);
       }
+    } catch (err) {
+      console.error(err);
+    }
 
-      next();
-    });
+    next();
   },
 
-  pageLoaded: function(req, res, next) {
+  pageLoaded: async function(req, res, next) {
     if(req.prerender.statusCode !== 200) {
       return next();
     }
 
     const s3Options = {
+      Bucket: process.env.S3_BUCKET_NAME,
       Key: this.getCacheKey(req),
       ContentType: 'text/html;charset=UTF-8',
       StorageClass: 'REDUCED_REDUNDANCY',
       Body: req.prerender.content
     }
 
-    s3.putObject(s3Options, (err, result) => {
-      if (err) console.error(err);
-      next();
-    });
+    try {
+      await s3.putObject(s3Options);
+    } catch (err) {
+      console.error(err);
+    }
+
+    next();
   },
 
-  getCacheKey: function(req) {  
+  getCacheKey: function(req) {
     let key = req.prerender.url;
     if (req.prerender.width) {
       key = `${key}-width-${req.prerender.width}`;
     }
-    
+
     const optionsObj = url.parse(req.url, true).query;
     if ('viewerType' in optionsObj) {
       key = `${optionsObj.viewerType}/${key}`;
